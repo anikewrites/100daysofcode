dd668cfc6c9d6c2c007aa27d1504fc98681de2a0
anikewrites <37543971+anikewrites@users.noreply.github.com>
2020-06-13 11:51:44 -0400

    Add files via upload

---
+++ Code1.py
@@ -0,0 +1,15 @@
+#This programs says hello and asks for my name
+
+print("Hello and Welcome to my program for the first day of my 100 days of code challenge")
+
+print("What is your name?")
+myName = input()
+print("Your name is " + myName + ", Nice to meet you!")
+
+print("The length of your name is: ")
+print(len(myName))
+
+print('How old are you?')
+myAge = input()
+
+print("You will " + str(int(myAge) + 1) + " in a year")
